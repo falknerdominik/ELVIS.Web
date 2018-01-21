eccc7f178eab36b3e0f0ea2bff38df82ca838dab
Falkner Dominik <falkner.dominik@gmail.com>
2018-01-21 19:04:08 +0100

    Fix naviation highlighting

--- src/app/view/components/menu/menu.component.ts
+++ src/app/view/components/menu/menu.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SettingsService } from '../../../library/services/settings-service.service';
 import { User } from '../../../library/api/index';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'menu',
@@ -9,11 +11,16 @@ import { User } from '../../../library/api/index';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  action: any;
 
   user: User;
 
-  constructor(private router: Router, private settingsService: SettingsService) { 
+  constructor(private router: Router, private settingsService: SettingsService, private route: ActivatedRoute) { 
     this.user = this.settingsService.getUser();
+    console.log(router.url);
+    this.route.queryParams.subscribe(
+      p => this.action = p.area
+    );
   }
 
   ngOnInit() {
